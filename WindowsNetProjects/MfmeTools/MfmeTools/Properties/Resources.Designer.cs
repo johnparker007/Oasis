7d7cf94c82394e84abcb3af9ec7453bf0b711771
John Parker <gyrox007@gmail.com>
2024-05-02 21:34:45 +0100

    MfmeTools - scraper - add delphi font source image to Resources system

--- WindowsNetProjects/MfmeTools/MfmeTools/Properties/Resources.Designer.cs
+++ WindowsNetProjects/MfmeTools/MfmeTools/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace MfmeTools.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap DelphiFontScraper_sourceImage {
+            get {
+                object obj = ResourceManager.GetObject("DelphiFontScraper_sourceImage", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
