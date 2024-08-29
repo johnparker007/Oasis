6b8ecb30152a6537e7a774b50cde845ac63bca3f
John Parker <gyrox007@gmail.com>
2024-08-29 22:09:04 +0100

    Test changes to get TMP fonts rendering via SDF
    Kinda hacked in for now, but does provide similar level of clarity/layout to MFME, but with 'infinite zoom' rather than bitmap via signed distance field and some tunings.

--- UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/LayoutEditor/Components/EditorComponentLamp.cs
+++ UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/LayoutEditor/Components/EditorComponentLamp.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using Oasis.Layout;
 using Oasis.Graphics;
 using Oasis.UI;
+using TMPro;
 
 namespace Oasis.LayoutEditor
 {
@@ -18,6 +19,7 @@ namespace Oasis.LayoutEditor
         private Sprite _sprite = null;
         private Texture2D _texture2d = null;
         private Text _text = null;
+        private TMPro.TMP_Text _tmpText = null;
         private Outline _outline = null;
 
         protected ComponentLamp ComponentLamp
@@ -34,6 +36,7 @@ namespace Oasis.LayoutEditor
 
             _image = GetComponent<Image>();
             _text = GetComponentInChildren<Text>();
+            _tmpText = GetComponentInChildren<TMPro.TMP_Text>();
             _outline = GetComponent<Outline>();
         }
 
@@ -45,6 +48,10 @@ namespace Oasis.LayoutEditor
 
             _text.text = ComponentLamp.Text;
             _text.color = ComponentLamp.TextColor;
+
+            _tmpText.text = ComponentLamp.Text;
+            _tmpText.color = ComponentLamp.TextColor;
+
 
             // TEMP hack test for these fonts just to get things going!
 
@@ -58,7 +65,28 @@ namespace Oasis.LayoutEditor
                 _text.font = font;
                 _text.fontSize = fontSize;
                 _text.fontStyle = fontStyle; // TODO do we not need this if baked into the font we gewnerated?
+
+                // more work to be done, can get the Unispace font to kinda work, provided don't use Italic 
+                //_text.fontStyle = FontStyle.Normal;
+
                 // otherwise we may end up with 'double-bold' fonts or double-italic fonts.
+
+
+                // *** New TMP version:
+                TMP_FontAsset fontAsset = TMP_FontAsset.CreateFontAsset(font);
+                _tmpText.font = fontAsset;
+                //float fontSizeFloat = ComponentLamp.FontSize * kMfmeFontScale;
+// test round down to int:
+float fontSizeFloat = (int)(ComponentLamp.FontSize * kMfmeFontScale);
+                _tmpText.fontSize = fontSizeFloat;
+
+                // settings to give a less blurry/soft font at smaller sizes
+                // (in time this may want to vary with font size, but these are a good start)
+                _tmpText.font.material.SetFloat("_Sharpness", 1f);
+                _tmpText.font.material.SetFloat("_GradientScale", 15f);
+
+
+                //_tmpText.fontStyle = fontStyle;
             }
 
             // TODO THERE ARE POTENTIALLY IMAGE-RELATED MEMORY LEAKS TO FIX HERE!
