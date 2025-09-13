e822d51dc7f19883a0f91e238d91df7512ad4a84
John Parker <gyrox007@gmail.com>
2025-09-13 20:54:47 +0100

    Make Maximise option toggleable

--- UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/ContextMenus/TabHeaderContextMenu.cs
+++ UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/ContextMenus/TabHeaderContextMenu.cs
@@ -6,13 +6,23 @@ using DynamicPanels;
 // Context menu for tab headers.
 public sealed class TabHeaderContextMenu : NativeContextMenu
 {
+    bool _maximised;
+
     protected override List<NativeContextMenuManager.MenuItemSpec> BuildMenu()
     {
         var tab = GetComponent<PanelTab>();
 
         return new List<NativeContextMenuManager.MenuItemSpec>
         {
-            new NativeContextMenuManager.MenuItemSpec("Maximise", () => Debug.Log("Maximise clicked")),
+            new NativeContextMenuManager.MenuItemSpec(
+                "Maximise",
+                () =>
+                {
+                    _maximised = !_maximised;
+                    Debug.Log($"Maximise set to {_maximised}");
+                },
+                true,
+                _maximised),
             new NativeContextMenuManager.MenuItemSpec(
                 "Close",
                 () =>
