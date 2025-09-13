3500087e8eabde829a0bc789334358f74e24b92c
John Parker <gyrox007@gmail.com>
2025-09-13 21:20:23 +0100

    Disable resize and drag when panel maximized

--- UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/ContextMenus/TabHeaderContextMenu.cs
+++ UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/ContextMenus/TabHeaderContextMenu.cs
@@ -8,6 +8,9 @@ public sealed class TabHeaderContextMenu : NativeContextMenu
 {
     bool _maximised;
     byte[] _savedBytes;
+    PanelHeader _panelHeader;
+    PanelResizeHelper[] _resizeHelpers;
+    PanelTab[] _tabs;
 
     protected override List<NativeContextMenuManager.MenuItemSpec> BuildMenu()
     {
@@ -33,9 +36,60 @@ public sealed class TabHeaderContextMenu : NativeContextMenu
                         panel.BringForward();
                         panel.RectTransform.anchoredPosition = Vector2.zero;
                         panel.FloatingSize = panel.Canvas.Size;
+
+                        _panelHeader = panel.GetComponentInChildren<PanelHeader>();
+                        if (_panelHeader)
+                        {
+                            _panelHeader.enabled = false;
+                        }
+
+                        _resizeHelpers = panel.GetComponentsInChildren<PanelResizeHelper>();
+                        foreach (var helper in _resizeHelpers)
+                        {
+                            if (helper)
+                            {
+                                helper.enabled = false;
+                            }
+                        }
+
+                        _tabs = panel.GetComponentsInChildren<PanelTab>();
+                        foreach (var t in _tabs)
+                        {
+                            if (t)
+                            {
+                                t.enabled = false;
+                            }
+                        }
                     }
                     else
                     {
+                        if (_panelHeader)
+                        {
+                            _panelHeader.enabled = true;
+                        }
+
+                        if (_resizeHelpers != null)
+                        {
+                            foreach (var helper in _resizeHelpers)
+                            {
+                                if (helper)
+                                {
+                                    helper.enabled = true;
+                                }
+                            }
+                        }
+
+                        if (_tabs != null)
+                        {
+                            foreach (var t in _tabs)
+                            {
+                                if (t)
+                                {
+                                    t.enabled = true;
+                                }
+                            }
+                        }
+
                         PanelSerialization.DeserializeCanvasFromArray(panel.Canvas, _savedBytes);
                     }
                 },
