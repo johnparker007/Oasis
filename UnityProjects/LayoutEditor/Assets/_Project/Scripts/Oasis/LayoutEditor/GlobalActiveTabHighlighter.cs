a253ce164a21be9b6577f8043d136521c49adb0a
John Parker <gyrox007@gmail.com>
2025-09-13 18:07:13 +0100

    Add global active tab highlighter

---
+++ UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/LayoutEditor/GlobalActiveTabHighlighter.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DynamicPanels;
+
+namespace Oasis.LayoutEditor
+{
+    public class GlobalActiveTabHighlighter : MonoBehaviour
+    {
+        [SerializeField]
+        private Color _highlightColor = Color.yellow;
+
+        [SerializeField]
+        private float _highlightThickness = 2f;
+
+        private PanelTab _current;
+
+        private void OnEnable()
+        {
+            PanelNotificationCenter.OnActiveTabChanged += OnActiveTabChanged;
+            PanelNotificationCenter.OnTabCreated += OnTabCreated;
+            PanelNotificationCenter.OnTabDestroyed += OnTabDestroyed;
+        }
+
+        private void OnDisable()
+        {
+            PanelNotificationCenter.OnActiveTabChanged -= OnActiveTabChanged;
+            PanelNotificationCenter.OnTabCreated -= OnTabCreated;
+            PanelNotificationCenter.OnTabDestroyed -= OnTabDestroyed;
+        }
+
+        private void OnTabCreated(PanelTab tab)
+        {
+            Outline outline = tab.gameObject.GetComponent<Outline>();
+            if (outline == null)
+                outline = tab.gameObject.AddComponent<Outline>();
+
+            outline.effectColor = _highlightColor;
+            outline.effectDistance = new Vector2(_highlightThickness, _highlightThickness);
+            outline.enabled = false;
+        }
+
+        private void OnActiveTabChanged(PanelTab tab)
+        {
+            if (_current != null)
+            {
+                Outline currentOutline = _current.gameObject.GetComponent<Outline>();
+                if (currentOutline != null)
+                    currentOutline.enabled = false;
+            }
+
+            _current = tab;
+
+            if (_current != null)
+            {
+                Outline newOutline = _current.gameObject.GetComponent<Outline>();
+                if (newOutline == null)
+                    newOutline = _current.gameObject.AddComponent<Outline>();
+
+                newOutline.effectColor = _highlightColor;
+                newOutline.effectDistance = new Vector2(_highlightThickness, _highlightThickness);
+                newOutline.enabled = true;
+            }
+        }
+
+        private void OnTabDestroyed(PanelTab tab)
+        {
+            if (_current == tab)
+            {
+                Outline outline = tab.gameObject.GetComponent<Outline>();
+                if (outline != null)
+                    outline.enabled = false;
+
+                _current = null;
+            }
+        }
+    }
+}
