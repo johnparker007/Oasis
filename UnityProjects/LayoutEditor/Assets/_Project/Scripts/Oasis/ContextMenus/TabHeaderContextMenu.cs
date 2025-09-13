3d0defaae098efe5b0e1e69ebf2b5eda99b7577d
John Parker <gyrox007@gmail.com>
2025-09-13 20:38:51 +0100

    Implement tab close via context menu

--- UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/ContextMenus/TabHeaderContextMenu.cs
+++ UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/ContextMenus/TabHeaderContextMenu.cs
@@ -1,16 +1,27 @@
 using NativeWindowsContextMenu;
 using System.Collections.Generic;
 using UnityEngine;
+using DynamicPanels;
 
 // Context menu for tab headers.
 public sealed class TabHeaderContextMenu : NativeContextMenu
 {
     protected override List<NativeContextMenuManager.MenuItemSpec> BuildMenu()
     {
+        var tab = GetComponent<PanelTab>();
+
         return new List<NativeContextMenuManager.MenuItemSpec>
         {
             new NativeContextMenuManager.MenuItemSpec("Maximise", () => Debug.Log("Maximise clicked")),
-            new NativeContextMenuManager.MenuItemSpec("Close", () => Debug.Log("Close clicked")),
+            new NativeContextMenuManager.MenuItemSpec(
+                "Close",
+                () =>
+                {
+                    if (tab)
+                    {
+                        tab.Destroy();
+                    }
+                }),
         };
     }
 }
