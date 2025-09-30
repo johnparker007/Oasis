e3302ec9009865554e3eeff8448ac79772024cec
John Parker <gyrox007@gmail.com>
2025-09-30 14:06:28 +0100

    Editor Preferences menu (new approach using rt hierarchy
    - this will have a hierarchy as left pane navigation with General / MAME / etc sections, each enabling a linked window on the right with those prefs

---
+++ UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/LayoutEditor/Panels/PanelEditorPreferencesNew.cs
@@ -0,0 +1,40 @@
+using Oasis.LayoutEditor.RuntimeHierarchyIntegration;
+using RuntimeInspectorNamespace;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.EventSystems;
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+using NativeWindowsContextMenu;
+using System.Diagnostics;
+#endif
+
+namespace Oasis.LayoutEditor.Panels
+{
+    public class PanelEditorPreferencesNew : PanelBase
+    {
+        private const string kPseudoSceneName = "Preferences";
+
+        protected override void AddListeners()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Initialise()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Populate()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RemoveListeners()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
