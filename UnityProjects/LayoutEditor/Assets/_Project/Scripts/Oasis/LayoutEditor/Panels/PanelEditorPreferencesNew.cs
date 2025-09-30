09b5d7d113592987c9d91d67fd484286492a9a44
John Parker <gyrox007@gmail.com>
2025-09-30 14:18:37 +0100

    Add preferences hierarchy sections and selection logging

--- UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/LayoutEditor/Panels/PanelEditorPreferencesNew.cs
+++ UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/LayoutEditor/Panels/PanelEditorPreferencesNew.cs
@@ -1,14 +1,7 @@
-using Oasis.LayoutEditor.RuntimeHierarchyIntegration;
 using RuntimeInspectorNamespace;
-using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Collections.ObjectModel;
 using UnityEngine;
-using UnityEngine.EventSystems;
-#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
-using NativeWindowsContextMenu;
-using System.Diagnostics;
-#endif
 
 namespace Oasis.LayoutEditor.Panels
 {
@@ -16,24 +9,160 @@ namespace Oasis.LayoutEditor.Panels
     {
         private const string kPseudoSceneName = "Preferences";
 
+        private static readonly string[] kInitialSections =
+        {
+            "General",
+            "MAME"
+        };
+
+        private RuntimeHierarchy _runtimeHierarchy;
+        private readonly List<Transform> _sectionTransforms = new List<Transform>();
+        private readonly Dictionary<Transform, string> _sectionNamesByTransform = new Dictionary<Transform, string>();
+
         protected override void AddListeners()
         {
-            throw new NotImplementedException();
+            EnsureRuntimeHierarchy();
+
+            if (_runtimeHierarchy != null)
+            {
+                _runtimeHierarchy.OnSelectionChanged -= OnRuntimeHierarchySelectionChanged;
+                _runtimeHierarchy.OnSelectionChanged += OnRuntimeHierarchySelectionChanged;
+            }
         }
 
         protected override void Initialise()
         {
-            throw new NotImplementedException();
+            if (_initialised)
+            {
+                return;
+            }
+
+            EnsureRuntimeHierarchy();
+
+            if (_runtimeHierarchy != null)
+            {
+                _runtimeHierarchy.CreatePseudoScene(kPseudoSceneName);
+            }
+
+            _initialised = true;
         }
 
         protected override void Populate()
         {
-            throw new NotImplementedException();
+            CreatePreferenceSections();
         }
 
         protected override void RemoveListeners()
         {
-            throw new NotImplementedException();
+            if (_runtimeHierarchy != null)
+            {
+                _runtimeHierarchy.OnSelectionChanged -= OnRuntimeHierarchySelectionChanged;
+            }
+
+            ClearPreferenceSections();
+        }
+
+        private void EnsureRuntimeHierarchy()
+        {
+            if (_runtimeHierarchy == null)
+            {
+                _runtimeHierarchy = GetComponentInChildren<RuntimeHierarchy>(true);
+            }
+        }
+
+        private void CreatePreferenceSections()
+        {
+            EnsureRuntimeHierarchy();
+
+            if (_runtimeHierarchy == null)
+            {
+                return;
+            }
+
+            ClearPreferenceSections();
+
+            foreach (string sectionName in kInitialSections)
+            {
+                Transform sectionTransform = CreateSectionTransform(sectionName);
+                _runtimeHierarchy.AddToPseudoScene(kPseudoSceneName, sectionTransform);
+
+                _sectionTransforms.Add(sectionTransform);
+                _sectionNamesByTransform[sectionTransform] = sectionName;
+            }
+        }
+
+        private Transform CreateSectionTransform(string sectionName)
+        {
+            GameObject sectionObject = new GameObject(sectionName)
+            {
+                hideFlags = HideFlags.HideAndDontSave
+            };
+
+            return sectionObject.transform;
+        }
+
+        private void ClearPreferenceSections()
+        {
+            if (_sectionTransforms.Count > 0)
+            {
+                if (_runtimeHierarchy != null)
+                {
+                    foreach (Transform sectionTransform in _sectionTransforms)
+                    {
+                        _runtimeHierarchy.RemoveFromPseudoScene(kPseudoSceneName, sectionTransform, false);
+                    }
+                }
+
+                foreach (Transform sectionTransform in _sectionTransforms)
+                {
+                    DestroyTransform(sectionTransform);
+                }
+            }
+
+            _sectionTransforms.Clear();
+            _sectionNamesByTransform.Clear();
+        }
+
+        private void DestroyTransform(Transform transform)
+        {
+            if (transform == null)
+            {
+                return;
+            }
+
+            if (Application.isPlaying)
+            {
+                Destroy(transform.gameObject);
+            }
+            else
+            {
+                DestroyImmediate(transform.gameObject);
+            }
+        }
+
+        private void OnRuntimeHierarchySelectionChanged(ReadOnlyCollection<Transform> selection)
+        {
+            if (selection == null || selection.Count == 0)
+            {
+                return;
+            }
+
+            for (int i = selection.Count - 1; i >= 0; i--)
+            {
+                Transform selectedTransform = selection[i];
+
+                if (selectedTransform != null &&
+                    _sectionNamesByTransform.TryGetValue(selectedTransform, out string sectionName))
+                {
+                    OnPreferenceSectionSelected(sectionName);
+                    return;
+                }
+            }
+        }
+
+        private void OnPreferenceSectionSelected(string sectionName)
+        {
+            Debug.Log($"Preference section selected: {sectionName}");
         }
     }
 
