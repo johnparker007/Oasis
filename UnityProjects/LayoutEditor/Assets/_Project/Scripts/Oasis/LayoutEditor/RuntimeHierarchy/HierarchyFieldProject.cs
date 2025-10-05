57b495d3d77d547a35ca784ddb28933d3da883b2
John Parker <gyrox007@gmail.com>
2025-10-05 12:04:32 +0100

    Hierarchy field - project; set up refs for folder icon/source sprites

--- UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/LayoutEditor/RuntimeHierarchy/HierarchyFieldProject.cs
+++ UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/LayoutEditor/RuntimeHierarchy/HierarchyFieldProject.cs
@@ -1,7 +1,13 @@
 using RuntimeInspectorNamespace;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HierarchyFieldProject : HierarchyField
 {
+    [SerializeField] private Image _folderIconImage;
 
+    [SerializeField] private Sprite _closedFolderEmptySprite;
+    [SerializeField] private Sprite _closedFolderNonEmptySprite;
+    [SerializeField] private Sprite _openFolderNonEmptySprite;
+    
 }
