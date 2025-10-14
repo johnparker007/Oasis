e8182aaea85182f9340c160d821f5c430ac0c32f
John Parker <gyrox007@gmail.com>
2025-10-14 16:16:55 +0100

    Wrap ntivedialog test/example in debug flag (disabled)
    Will remove once start using these in other places

--- UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/Projects/ProjectsController.cs
+++ UnityProjects/LayoutEditor/Assets/_Project/Scripts/Oasis/Projects/ProjectsController.cs
@@ -91,24 +91,28 @@ namespace Oasis.Projects
             Import.Importer importer = new Import.Importer();
             Editor.Instance.Project = importer.Import(projectJsonPath);
 
+            const bool kDebugNativeDialogTest = false;
+            if(kDebugNativeDialogTest)
+            {
 #if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
-            var dialogOptions = new NativeDialogOptions
-            {
-                Title = "Project Loaded",
-                Message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                ShowOkButton = true,
-                ShowCancelButton = true,
-                ShowCloseButton = true,
-                Icon = NativeDialogIcon.Information,
-                OnOkClicked = () => Debug.Log("OK Clicked"),
-                OnCancelClicked = () => Debug.Log("Cancel Clicked"),
-            };
+                var dialogOptions = new NativeDialogOptions
+                {
+                    Title = "Project Loaded",
+                    Message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    ShowOkButton = true,
+                    ShowCancelButton = true,
+                    ShowCloseButton = true,
+                    Icon = NativeDialogIcon.Information,
+                    OnOkClicked = () => Debug.Log("OK Clicked"),
+                    OnCancelClicked = () => Debug.Log("Cancel Clicked"),
+                };
 
-            if (!NativeDialogWindow.ShowDialog(dialogOptions, out string dialogErrorMessage) && !string.IsNullOrEmpty(dialogErrorMessage))
-            {
-                Debug.LogWarning($"Failed to display native dialog: {dialogErrorMessage}");
-            }
+                if (!NativeDialogWindow.ShowDialog(dialogOptions, out string dialogErrorMessage) && !string.IsNullOrEmpty(dialogErrorMessage))
+                {
+                    Debug.LogWarning($"Failed to display native dialog: {dialogErrorMessage}");
+                }
 #endif
+            }
 
             return true;
         }
