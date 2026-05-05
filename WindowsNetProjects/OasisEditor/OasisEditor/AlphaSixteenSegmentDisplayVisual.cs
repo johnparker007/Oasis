40e495fd7e2e64f12c4f00836d48ccde2b6861e6
John Parker <gyrox007@gmail.com>
2026-05-05 13:04:57 +0100

    Add seven-segment vector display rendering and shared visual base

--- WindowsNetProjects/OasisEditor/OasisEditor/AlphaSixteenSegmentDisplayVisual.cs
+++ WindowsNetProjects/OasisEditor/OasisEditor/AlphaSixteenSegmentDisplayVisual.cs
@@ -1,88 +1,13 @@
-using System.Windows;
-using System.Windows.Media;
-
 namespace OasisEditor;
 
-internal sealed class AlphaSixteenSegmentDisplayVisual : FrameworkElement
+internal sealed class AlphaSixteenSegmentDisplayVisual : SegmentDisplayVisualBase
 {
-    private static readonly Pen SegmentPen = CreateSegmentPen();
-    private readonly SegmentDisplayDefinition? _definition;
-
     public AlphaSixteenSegmentDisplayVisual()
+        : base(LoadDefinition())
     {
-        SegmentDisplayDefinitionLoader.TryGetDefinition(out var definition);
-        _definition = definition;
-        ClipToBounds = true;
-        SnapsToDevicePixels = true;
     }
 
-    public int CellCount { get; set; } = 16;
-
-    public Brush LitBrush { get; set; } = Brushes.OrangeRed;
-
-    public Brush UnlitBrush { get; set; } = new SolidColorBrush(Color.FromArgb(120, 255, 69, 0));
-
-    public string? DisplayText { get; set; }
-
-    public bool ShowDecimalPoint { get; set; }
-
-    protected override void OnRender(DrawingContext drawingContext)
-    {
-        base.OnRender(drawingContext);
-
-        if (_definition?.Cell?.Size is null || _definition.Cell.Segments is null)
-        {
-            return;
-        }
-
-        var cellSize = _definition.Cell.Size.AsSize;
-        if (cellSize.Width <= 0 || cellSize.Height <= 0 || ActualWidth <= 0 || ActualHeight <= 0)
-        {
-            return;
-        }
-
-        var pitch = _definition.Cell.RecommendedPitch <= 0 ? cellSize.Width : _definition.Cell.RecommendedPitch;
-        var sourceWidth = pitch * CellCount;
-        var scale = Math.Min(ActualWidth / sourceWidth, ActualHeight / cellSize.Height);
-        var offsetX = (ActualWidth - (sourceWidth * scale)) * 0.5;
-        var offsetY = (ActualHeight - (cellSize.Height * scale)) * 0.5;
-
-        for (var i = 0; i < CellCount; i++)
-        {
-            var charIndex = i < (DisplayText?.Length ?? 0) ? i : -1;
-            var segmentMask = charIndex >= 0 ? GetBasicMaskForChar(DisplayText![charIndex]) : 0;
-            var cellTransform = new MatrixTransform(scale, 0, 0, scale, offsetX + (i * pitch * scale), offsetY);
-
-            foreach (var segment in _definition.Cell.Segments)
-            {
-                if (segment.Geometry is null)
-                {
-                    continue;
-                }
-
-                var lit = (segmentMask & (1 << segment.Index)) != 0;
-                drawingContext.PushTransform(cellTransform);
-                drawingContext.DrawGeometry(lit ? LitBrush : UnlitBrush, SegmentPen, segment.Geometry);
-                drawingContext.Pop();
-            }
-
-            if (ShowDecimalPoint && _definition.Cell.DecimalPoint?.Geometry is not null)
-            {
-                drawingContext.PushTransform(cellTransform);
-                drawingContext.DrawGeometry(UnlitBrush, SegmentPen, _definition.Cell.DecimalPoint.Geometry);
-                drawingContext.Pop();
-            }
-        }
-    }
-
-    private static Pen CreateSegmentPen()
-    {
-        var pen = new Pen(new SolidColorBrush(Color.FromArgb(120, 18, 18, 18)), 0.4);
-        pen.Freeze();
-        return pen;
-    }
-
-    private static int GetBasicMaskForChar(char c)
+    protected override int GetSegmentMaskForChar(char c)
     {
         return char.ToUpperInvariant(c) switch
         {
@@ -90,5 +15,11 @@ internal sealed class AlphaSixteenSegmentDisplayVisual : FrameworkElement
             >= 'A' and <= 'Z' => 0b1111_1111_0000_1111,
             _ => 0
         };
+    }
+
+    private static SegmentDisplayDefinition? LoadDefinition()
+    {
+        SegmentDisplayDefinitionLoader.TryGetSixteenSegmentDefinition(out var definition);
+        return definition;
     }
 }
