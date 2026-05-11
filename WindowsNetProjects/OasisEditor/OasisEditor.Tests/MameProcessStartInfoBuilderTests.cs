5566e6afad11357fc8b8db1dad58d8f7a5a38fd2
John Parker <gyrox007@gmail.com>
2026-05-11 20:59:21 +0100

    Add MAME process start-info builder and tests

---
+++ WindowsNetProjects/OasisEditor/OasisEditor.Tests/MameProcessStartInfoBuilderTests.cs
@@ -0,0 +1,66 @@
+using Xunit;
+
+namespace OasisEditor.Tests;
+
+public sealed class MameProcessStartInfoBuilderTests
+{
+    [Fact]
+    public void Build_IncludesRomAndPluginArguments()
+    {
+        var builder = new MameProcessStartInfoBuilder();
+        var request = new MameProcessLaunchRequest(
+            @"C:\Mame\mame.exe",
+            "myrom",
+            @"C:\Users\john\AppData\Local\OasisEditor\MAME\roms",
+            @"C:\Mame\plugins",
+            string.Empty);
+
+        var startInfo = builder.Build(request);
+
+        Assert.Equal(@"C:\Mame\mame.exe", startInfo.FileName);
+        Assert.Contains("myrom", startInfo.Arguments);
+        Assert.Contains("-rompath", startInfo.Arguments);
+        Assert.Contains(@"C:\Users\john\AppData\Local\OasisEditor\MAME\roms", startInfo.Arguments);
+        Assert.Contains("-plugin oasis", startInfo.Arguments);
+        Assert.Contains("-plugins_path", startInfo.Arguments);
+        Assert.Contains(@"C:\Mame\plugins", startInfo.Arguments);
+    }
+
+    [Fact]
+    public void Build_AppendsAdditionalArguments()
+    {
+        var builder = new MameProcessStartInfoBuilder();
+        var request = new MameProcessLaunchRequest(
+            @"C:\Mame\mame.exe",
+            "myrom",
+            @"C:\roms",
+            @"C:\plugins",
+            "-window -verbose");
+
+        var startInfo = builder.Build(request);
+
+        Assert.EndsWith("-window -verbose", startInfo.Arguments);
+    }
+
+    [Fact]
+    public void Build_ConfiguresHiddenRedirectedProcess()
+    {
+        var builder = new MameProcessStartInfoBuilder();
+        var request = new MameProcessLaunchRequest(
+            @"C:\Mame\mame.exe",
+            "myrom",
+            @"C:\roms",
+            @"C:\plugins",
+            string.Empty);
+
+        var startInfo = builder.Build(request);
+
+        Assert.False(startInfo.UseShellExecute);
+        Assert.True(startInfo.RedirectStandardInput);
+        Assert.True(startInfo.RedirectStandardOutput);
+        Assert.True(startInfo.RedirectStandardError);
+        Assert.True(startInfo.CreateNoWindow);
+        Assert.Equal(System.Diagnostics.ProcessWindowStyle.Hidden, startInfo.WindowStyle);
+        Assert.Equal(@"C:\Mame", startInfo.WorkingDirectory);
+    }
+}
