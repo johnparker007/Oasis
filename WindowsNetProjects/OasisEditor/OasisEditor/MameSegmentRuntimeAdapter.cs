fa5df82cce0ceb549fb249b2d90109027ca4761d
John Parker <gyrox007@gmail.com>
2026-05-16 18:53:16 +0100

    Fix alpha segment bit mapping for MAME VFD output

--- WindowsNetProjects/OasisEditor/OasisEditor/MameSegmentRuntimeAdapter.cs
+++ WindowsNetProjects/OasisEditor/OasisEditor/MameSegmentRuntimeAdapter.cs
@@ -2,6 +2,25 @@ namespace OasisEditor;
 
 public sealed class MameSegmentRuntimeAdapter : IMameSegmentRuntimeAdapter
 {
+    private static readonly int[] LegacyMameBitToWpfSegmentIndexMap =
+    [
+        2,  // 0: top-left bar
+        3,  // 1: top-right bar
+        1,  // 2: right-top bar
+        4,  // 3: right-bottom bar
+        7,  // 4: bottom-right bar
+        6,  // 5: bottom-left bar
+        5,  // 6: left-bottom bar
+        0,  // 7: left-top bar
+        11, // 8: horizontal-middle-left bar
+        12, // 9: horizontal-middle-right bar
+        15, // 10: vertical-middle-top bar
+        8,  // 11: vertical-middle-bottom bar
+        9,  // 12: diagonal-left-bottom bar
+        14, // 13: diagonal-left-top bar
+        13, // 14: diagonal-right-top bar
+        10  // 15: diagonal-right-bottom bar
+    ];
     private readonly object _pendingSync = new();
     private readonly Func<IEnumerable<DocumentTabViewModel>> _documentProvider;
     private readonly Action<Action> _uiDispatch;
@@ -55,7 +74,7 @@ public sealed class MameSegmentRuntimeAdapter : IMameSegmentRuntimeAdapter
                 {
                     if (_latestMasksByCell.TryGetValue(baseIndex + i, out var mask))
                     {
-                        cellMasks[i] = mask;
+                        cellMasks[i] = RemapMameMaskToWpfSegmentOrder(mask);
                     }
                 }
 
@@ -70,5 +89,23 @@ public sealed class MameSegmentRuntimeAdapter : IMameSegmentRuntimeAdapter
                 document.NotifyPanelVisualPreviewChanged(changedObjectIds);
             }
         }
+    }
+
+    private static int RemapMameMaskToWpfSegmentOrder(int rawMask)
+    {
+        var normalizedMask = rawMask & 0xFFFF;
+        var remappedMask = 0;
+
+        for (var bit = 0; bit < LegacyMameBitToWpfSegmentIndexMap.Length; bit++)
+        {
+            if ((normalizedMask & (1 << bit)) == 0)
+            {
+                continue;
+            }
+
+            remappedMask |= 1 << LegacyMameBitToWpfSegmentIndexMap[bit];
+        }
+
+        return remappedMask;
     }
 }
