ab8647daa8c05c9b0c4ca8070e59628c4538d410
John Parker <gyrox007@gmail.com>
2026-06-03 18:21:47 +0100

    Add Epoch MAME input port mapping

--- WindowsNetProjects/OasisEditor/OasisEditor.Tests/MameInputPortResolverTests.cs
+++ WindowsNetProjects/OasisEditor/OasisEditor.Tests/MameInputPortResolverTests.cs
@@ -30,6 +30,25 @@ public sealed class MameInputPortResolverTests
         Assert.Equal("4", target.Mask);
     }
 
+    [Theory]
+    [InlineData("0", "IN0", "1")]
+    [InlineData("17", "COINS", "2")]
+    [InlineData("24", "STAKE", "1")]
+    [InlineData("48", "CAB1", "1")]
+    [InlineData("57", "CAB2", "2")]
+    [InlineData("64", "DSW1", "1")]
+    [InlineData("73", "DSW2", "2")]
+    public void TryResolve_EpochButton_ResolvesTagAndMask(string buttonNumber, string expectedTag, string expectedMask)
+    {
+        var input = new InputDefinitionModel { Id = "1", Kind = InputDefinitionKind.Button, ButtonNumber = buttonNumber };
+
+        var ok = _resolver.TryResolve(FruitMachinePlatformType.Epoch, input, out var target);
+
+        Assert.True(ok);
+        Assert.Equal(expectedTag, target.Tag);
+        Assert.Equal(expectedMask, target.Mask);
+    }
+
     [Fact]
     public void TryResolve_UnsupportedPlatform_ReturnsFalse()
     {
