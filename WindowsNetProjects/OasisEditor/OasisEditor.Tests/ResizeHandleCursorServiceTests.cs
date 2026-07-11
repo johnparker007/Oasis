d0d63d1028cdcb4227d96ebd162b039beba6f597
John Parker <gyrox007@gmail.com>
2026-07-11 22:27:50 +0100

    Implement realtime panel resize previews

--- WindowsNetProjects/OasisEditor/OasisEditor.Tests/ResizeHandleCursorServiceTests.cs
+++ WindowsNetProjects/OasisEditor/OasisEditor.Tests/ResizeHandleCursorServiceTests.cs
@@ -5,17 +5,20 @@ namespace OasisEditor.Tests;
 public sealed class ResizeHandleCursorServiceTests
 {
     [Theory]
-    [InlineData(ResizeHandleKind.Left, ResizeHandleCursorKind.SizeWE)]
-    [InlineData(ResizeHandleKind.Right, ResizeHandleCursorKind.SizeWE)]
-    [InlineData(ResizeHandleKind.Top, ResizeHandleCursorKind.SizeNS)]
-    [InlineData(ResizeHandleKind.Bottom, ResizeHandleCursorKind.SizeNS)]
-    [InlineData(ResizeHandleKind.TopLeft, ResizeHandleCursorKind.SizeNWSE)]
-    [InlineData(ResizeHandleKind.BottomRight, ResizeHandleCursorKind.SizeNWSE)]
-    [InlineData(ResizeHandleKind.TopRight, ResizeHandleCursorKind.SizeNESW)]
-    [InlineData(ResizeHandleKind.BottomLeft, ResizeHandleCursorKind.SizeNESW)]
-    [InlineData(ResizeHandleKind.None, ResizeHandleCursorKind.Arrow)]
-    public void GetCursorKind_MapsResizeHandlesToConventionalDirections(ResizeHandleKind handleKind, ResizeHandleCursorKind expected)
+    [InlineData((int)ResizeHandleKind.Left, (int)ResizeHandleCursorKind.SizeWE)]
+    [InlineData((int)ResizeHandleKind.Right, (int)ResizeHandleCursorKind.SizeWE)]
+    [InlineData((int)ResizeHandleKind.Top, (int)ResizeHandleCursorKind.SizeNS)]
+    [InlineData((int)ResizeHandleKind.Bottom, (int)ResizeHandleCursorKind.SizeNS)]
+    [InlineData((int)ResizeHandleKind.TopLeft, (int)ResizeHandleCursorKind.SizeNWSE)]
+    [InlineData((int)ResizeHandleKind.BottomRight, (int)ResizeHandleCursorKind.SizeNWSE)]
+    [InlineData((int)ResizeHandleKind.TopRight, (int)ResizeHandleCursorKind.SizeNESW)]
+    [InlineData((int)ResizeHandleKind.BottomLeft, (int)ResizeHandleCursorKind.SizeNESW)]
+    [InlineData((int)ResizeHandleKind.None, (int)ResizeHandleCursorKind.Arrow)]
+    public void GetCursorKind_MapsResizeHandlesToConventionalDirections(int handleKindValue, int expectedValue)
     {
+        var handleKind = (ResizeHandleKind)handleKindValue;
+        var expected = (ResizeHandleCursorKind)expectedValue;
+
         Assert.Equal(expected, ResizeHandleCursorService.GetCursorKind(handleKind));
     }
 }
