8ff348fd37b89dc8c2783ac034e66409e1614776
John Parker <gyrox007@gmail.com>
2026-07-28 18:11:31 +0100

    Fix Amber Bridge interop namespace imports

--- WindowsNetProjects/OasisEditor/OasisEditor.Tests/AmberBridgeLibraryTests.cs
+++ WindowsNetProjects/OasisEditor/OasisEditor.Tests/AmberBridgeLibraryTests.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using OasisEditor;
 using Xunit;
 
 namespace OasisEditor.Tests;
--- WindowsNetProjects/OasisEditor/OasisEditor/Emulation/Native/AmberBridge/AmberBridgeNativeTypes.cs
+++ WindowsNetProjects/OasisEditor/OasisEditor/Emulation/Native/AmberBridge/AmberBridgeNativeTypes.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace OasisEditor;
