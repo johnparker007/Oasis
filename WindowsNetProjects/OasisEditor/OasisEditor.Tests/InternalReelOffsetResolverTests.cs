642a7e6ccdcd3db8a5f05bcc2ce1a7ea004d653a
John Parker <gyrox007@gmail.com>
2026-08-09 15:27:58 +0100

    Add Epoch 12-stop reel offset

--- WindowsNetProjects/OasisEditor/OasisEditor.Tests/InternalReelOffsetResolverTests.cs
+++ WindowsNetProjects/OasisEditor/OasisEditor.Tests/InternalReelOffsetResolverTests.cs
@@ -5,11 +5,19 @@ namespace OasisEditor.Tests;
 
 public sealed class InternalReelOffsetResolverTests
 {
+    [Fact]
+    public void ResolveNormalizedOffset_ReturnsExactEpochTwelveStopCorrection()
+    {
+        Assert.Equal(-0.16d, InternalReelOffsetResolver.ResolveNormalizedOffset(FruitMachinePlatformType.Epoch, 12));
+    }
+
     [Theory]
     [InlineData(FruitMachinePlatformType.MPU5, 16, -0.22d)]
     [InlineData(FruitMachinePlatformType.MPU5, 12, -0.075d)]
     [InlineData(FruitMachinePlatformType.MPU5, 24, 0d)]
     [InlineData(FruitMachinePlatformType.MPU4, 16, -0.05d)]
+    [InlineData(FruitMachinePlatformType.Epoch, 12, -0.16d)]
+    [InlineData(FruitMachinePlatformType.Epoch, 16, 0d)]
     [InlineData(FruitMachinePlatformType.Impact, 12, 0.025d)]
     [InlineData(FruitMachinePlatformType.Impact, 16, -0.018d)]
     [InlineData(FruitMachinePlatformType.Scorpion4, 12, 0.2d)]
--- WindowsNetProjects/OasisEditor/OasisEditor/InternalReelOffsetResolver.cs
+++ WindowsNetProjects/OasisEditor/OasisEditor/InternalReelOffsetResolver.cs
@@ -15,6 +15,7 @@ internal static class InternalReelOffsetResolver
             FruitMachinePlatformType.MPU4 when safeStops == 16 => -0.05d,
             FruitMachinePlatformType.MPU5 when safeStops == 12 => -0.075d,
             FruitMachinePlatformType.MPU5 when safeStops == 16 => -0.22d,
+            FruitMachinePlatformType.Epoch when safeStops == 12 => -0.16d,
             FruitMachinePlatformType.Impact when safeStops == 12 =>
                 ImpactTwelveStopBaseOffset + ImpactTwelveStopBandCorrection,
             FruitMachinePlatformType.Impact when safeStops == 16 =>
