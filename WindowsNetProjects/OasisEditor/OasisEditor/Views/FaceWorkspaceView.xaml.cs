722bdae54146a005626316c99eccacb106858d02
John Parker <gyrox007@gmail.com>
2026-08-22 18:04:44 +0100

    Face workspace: explicit modes, breadcrumbs, and calibration routing
    * Add explicit Face workspace editing modes
    
    * Fix breadcrumb XAML trigger structure

--- WindowsNetProjects/OasisEditor/OasisEditor/Views/FaceWorkspaceView.xaml.cs
+++ WindowsNetProjects/OasisEditor/OasisEditor/Views/FaceWorkspaceView.xaml.cs
@@ -1,11 +1,16 @@
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace OasisEditor.Views;
 
 public partial class FaceWorkspaceView : UserControl
 {
-    public FaceWorkspaceView()
+    public FaceWorkspaceView() => InitializeComponent();
+
+    private void OnPreviewKeyDown(object sender, KeyEventArgs eventArgs)
     {
-        InitializeComponent();
+        if (eventArgs.Key != Key.Escape || DataContext is not DocumentTabViewModel document || document.CalibrationPlacement is null) return;
+        document.CancelCalibrationPlacement();
+        eventArgs.Handled = true;
     }
 }
